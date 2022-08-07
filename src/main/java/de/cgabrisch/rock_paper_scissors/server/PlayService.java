712f6eec4a8a6c77689acd62db6e94c1e32fe403
Christian Gabrisch <github@cgabrisch.de>
2022-08-07 11:49:01 +0200

    Generate round id

--- src/main/java/de/cgabrisch/rock_paper_scissors/server/PlayService.java
+++ src/main/java/de/cgabrisch/rock_paper_scissors/server/PlayService.java
@@ -1,8 +1,11 @@
 package de.cgabrisch.rock_paper_scissors.server;
 
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +14,8 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class PlayService {
+    private final static Logger log = LoggerFactory.getLogger(PlayService.class);
+    
     private final AvailablePlayersService availablePlayersService;
     
     @Autowired
@@ -24,8 +29,10 @@ public class PlayService {
 
     private Mono<Round> playRound() {
         return Mono.fromFuture(availablePlayersService.checkOutPairOfPlayers()).map((players) -> {
+            String uuid = UUID.randomUUID().toString();
+            log.debug("Playing round {}", uuid);
             Calls calls = new Calls(Symbol.PAPER, Symbol.ROCK);
-            Round round = new Round(players.getT1(), players.getT2(), players.getT1(), calls, 1);
+            Round round = new Round(uuid, players.getT1(), players.getT2(), players.getT1(), calls, 1);
             
             availablePlayersService.checkInPlayer(players.getT1());
             availablePlayersService.checkInPlayer(players.getT2());
--- src/main/java/de/cgabrisch/rock_paper_scissors/server/Round.java
+++ src/main/java/de/cgabrisch/rock_paper_scissors/server/Round.java
@@ -1,5 +1,5 @@
 package de.cgabrisch.rock_paper_scissors.server;
 
-public record Round(Player player1, Player player2, Player winner, Calls calls, int stake) {
+public record Round(String roundId, Player player1, Player player2, Player winner, Calls calls, int stake) {
 
 }
